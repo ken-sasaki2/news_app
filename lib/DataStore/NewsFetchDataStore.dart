dc57ed1ff101ceeece0456e38d0e8c68859b5356
佐々木　謙 <nkekisasa222@gmail.com>
2023-03-22 23:01:19 +0900

    update: NewsFetchDataStoreを抽象化して、データ取得関数をNewsFetchResponseModelsを非同期で返すように書き換えた

--- lib/DataStore/NewsFetchDataStore.dart
+++ lib/DataStore/NewsFetchDataStore.dart
@@ -1,14 +1,31 @@
+
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import 'package:news_app/Application/Secret.dart';
+import 'package:news_app/Model/NewsFetchResponseModel.dart';
+import 'package:news_app/Model/NewsFetchResponseModels.dart';
 
-class NewsFetchDataStore {
+abstract class NewsFetchDataStoreInterface {
+  Future<NewsFetchResponseModels> fetchNewsData();
+}
 
-  void fetchNewsData() async {
+class NewsFetchDataStore extends NewsFetchDataStoreInterface {
+
+  @override
+  Future<NewsFetchResponseModels> fetchNewsData() async {
     final dio = Dio();
     const url = 'https://newsapi.org/v2/top-headlines?country=jp&apiKey=$key';
 
     final response = await dio.get(url);
-    debugPrint('response:$response');
+    final responseData = response.data;
+    final List<dynamic> datas = responseData['articles'];
+    final models = NewsFetchResponseModels();
+
+    datas.forEach((data) {
+      final model = NewsFetchResponseModel.fromData(data);
+      models.datas.add(model);
+    });
+
+    return models;
   }
 }
