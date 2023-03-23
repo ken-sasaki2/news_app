20fd18f1a080551d67360008c83b7e6a82395d8a
佐々木　謙 <nkekisasa222@gmail.com>
2023-03-23 17:54:30 +0900

    update: 例外処理の判定も必要なので`try catch`で囲んだ

--- lib/DataStore/NewsFetchDataStore.dart
+++ lib/DataStore/NewsFetchDataStore.dart
@@ -16,16 +16,21 @@ class NewsFetchDataStore extends NewsFetchDataStoreInterface {
     final dio = Dio();
     const url = 'https://newsapi.org/v2/top-headlines?country=jp&apiKey=$key';
 
-    final response = await dio.get(url);
-    final responseData = response.data;
-    final List<dynamic> datas = responseData['articles'];
-    final models = NewsFetchResponseModels();
+    try {
+      final response = await dio.get(url);
+      final responseData = response.data;
+      final List<dynamic> datas = responseData['articles'];
+      final models = NewsFetchResponseModels();
 
-    datas.forEach((data) {
-      final model = NewsFetchResponseModel.fromData(data);
-      models.datas.add(model);
-    });
+      datas.forEach((data) {
+        final model = NewsFetchResponseModel.fromData(data);
+        models.datas.add(model);
+      });
+      debugPrint('model:${models.datas[0].title}');
 
-    return models;
+      return models;
+    } catch(error) {
+      rethrow;
+    }
   }
 }
