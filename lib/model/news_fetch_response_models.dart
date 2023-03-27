9b8b296288d872e21a3b0645e8ef20ffe9e32987
佐々木　謙 <nkekisasa222@gmail.com>
2023-03-27 22:56:16 +0900

    update: ディレクトリ指定が間違ってた

--- lib/model/news_fetch_response_models.dart
+++ lib/model/news_fetch_response_models.dart
@@ -1,5 +1,5 @@
 
-import 'package:news_app/Model/news_fetch_response_model.dart';
+import 'package:news_app/model/news_fetch_response_model.dart';
 
 class NewsFetchResponseModels {
   List<NewsFetchResponseModel> datas = <NewsFetchResponseModel>[];
