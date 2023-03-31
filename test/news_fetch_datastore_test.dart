3daeb2d5177730081da5b3443ced59447a4b9d51
佐々木　謙 <nkekisasa222@gmail.com>
2023-03-31 21:09:12 +0900

    test: `NewsFetchDataStore `の正常にAPI通信のレスポンスが返ってくるテストと例外が返ってくるテストを書いた

--- test/news_fetch_datastore_test.dart
+++ test/news_fetch_datastore_test.dart
@@ -1,14 +1,22 @@
 
-import 'package:news_app/datastore/news_fetch_datastore.dart';
+import 'package:dio/dio.dart';
 import 'package:test/test.dart';
-
+import 'package:news_app/datastore/news_fetch_datastore.dart';
+import 'package:news_app/model/news_api_exception.dart';
 
   void main() {
     test('APIのレスポンスがnilではない', () async {
-      final dataStore = NewsFetchDataStore();
+      final dataStore = NewsFetchDataStore(dio: Dio());
       final data = await dataStore.fetchNewsData();
       final result = data.datas.isNotEmpty;
 
       expect(result, true);
+    });
+
+    test('NewsApiException型のExceptionが返ってくる', () async {
+      final dio = Dio(BaseOptions(validateStatus: (status) => false));
+      final dataStore = NewsFetchDataStore(dio: dio);
+
+      expect(() => dataStore.fetchNewsData(), throwsA(const TypeMatcher<NewsApiException>()));
     });
   }
