10c6ecf1c8e6709d7ece7a5554361ecd7b4d7080
佐々木　謙 <nkekisasa222@gmail.com>
2023-04-18 22:19:23 +0900

    update: Listの雛形を作成した

--- lib/view/news_widget.dart
+++ lib/view/news_widget.dart
@@ -5,6 +5,8 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:news_app/provider/news_view_model_provider.dart';
 
 class NewsWidget extends HookConsumerWidget {
+  const NewsWidget({super.key});
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final viewModel = ref.watch(newsViewModelNotifierProvider);
@@ -15,56 +17,56 @@ class NewsWidget extends HookConsumerWidget {
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('News'),
+          title: const Text('News'),
         ),
         body: ListView(
             children: [
-              _menuItem('サンプル1', Icon(Icons.abc_sharp)),
-              _menuItem('サンプル2', Icon(Icons.abc_sharp)),
-              _menuItem('サンプル3', Icon(Icons.abc_sharp)),
-              _menuItem('サンプル4', Icon(Icons.abc_sharp)),
-              _menuItem('サンプル5', Icon(Icons.abc_sharp)),
-              _menuItem('サンプル6', Icon(Icons.abc_sharp)),
-              _menuItem('サンプル7', Icon(Icons.abc_sharp)),
-              _menuItem('サンプル8', Icon(Icons.abc_sharp)),
-              _menuItem('サンプル9', Icon(Icons.abc_sharp)),
-              _menuItem('サンプル10', Icon(Icons.abc_sharp)),
-              _menuItem('サンプル11', Icon(Icons.abc_sharp)),
-              _menuItem('サンプル12', Icon(Icons.abc_sharp)),
-              _menuItem('サンプル13', Icon(Icons.abc_sharp)),
-              _menuItem('サンプル14', Icon(Icons.abc_sharp)),
-              _menuItem('サンプル15', Icon(Icons.abc_sharp))
+              newsItem('サンプル1', 'サブタイトル'),
+              newsItem('サンプル2', 'サブタイトル'),
+              newsItem('サンプル3', 'サブタイトル'),
+              newsItem('サンプル4', 'サブタイトル'),
+              newsItem('サンプル5', 'サブタイトル'),
+              newsItem('サンプル6', 'サブタイトル'),
+              newsItem('サンプル7', 'サブタイトル'),
+              newsItem('サンプル8', 'サブタイトル'),
+              newsItem('サンプル9', 'サブタイトル'),
+              newsItem('サンプル10', 'サブタイトル'),
+              newsItem('サンプル11', 'サブタイトル'),
+              newsItem('サンプル12', 'サブタイトル'),
+              newsItem('サンプル13', 'サブタイトル'),
+              newsItem('サンプル14', 'サブタイトル'),
+              newsItem('サンプル15', 'サブタイトル'),
             ],
         )
       ),
     );
   }
-  
-  Widget _menuItem(String title, Icon icon) {
+
+  Widget newsItem(String title, String author) {
     return GestureDetector(
       child:Container(
-          padding: EdgeInsets.all(8.0),
-          decoration: new BoxDecoration(
-              border: new Border(bottom: BorderSide(width: 1.0, color: Colors.grey))
+          padding: const EdgeInsets.all(12.0),
+          decoration: const BoxDecoration(
+              border: Border(bottom: BorderSide(color: Colors.grey, width: 1.0))
           ),
-          child: Row(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Container(
-                margin: EdgeInsets.all(10.0),
-                child:icon,
-              ),
-              Text(
-                title,
-                style: TextStyle(
-                    color:Colors.black,
-                    fontSize: 18.0
+              Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child:  Text(
+                  title, 
+                  style: const TextStyle(color:Colors.black, fontSize: 16.0),
                 ),
+              ),
+              Text(author,
+                style: const TextStyle(color:Colors.grey, fontSize: 12.0),
               ),
             ],
           )
       ),
       onTap: () {
-        print("onTap called.");
+        debugPrint("onTap called.");
       },
     );
   }
